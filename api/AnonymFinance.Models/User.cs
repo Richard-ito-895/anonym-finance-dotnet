f7061f7abfddbe254a76a40d9e024ead5cdb83e6
Ricardo Jaramillo <richardnator95@gmail.com>
2025-01-18 21:58:24 -0500

    Adding class library for models and referencing project

---
+++ api/AnonymFinance.Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace AnonymFinance.Models;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public required string Email { get; set; }
+    public required string PasswordHash { get; set; }
+    public long CreatedTs { get; set; }
+    public long LastUpddatedTs { get; set; }
+}
