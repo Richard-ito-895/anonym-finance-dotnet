5b6caf17f9bd5445847d170f4883f271bbca7257
Ricardo Jaramillo <richardnator95@gmail.com>
2025-01-18 22:10:36 -0500

    EF: First Migration

---
+++ api/AnonymFinanceAPI/Migrations/20250119030243_InitialCreateUsersTable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AnonymFinanceAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreateUsersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    PasswordHash = table.Column<string>(type: "text", nullable: false),
+                    CreatedTs = table.Column<long>(type: "bigint", nullable: false),
+                    LastUpddatedTs = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
